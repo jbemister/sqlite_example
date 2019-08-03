d3b7acb59b8425406c01ec541b3fcfb28f822f18
jbemister <bemiste1@msu.edu>
2019-08-02 21:19:55 -0400

    created tables and inserted entries into employee.db

--- sqlite.py
+++ sqlite.py
@@ -1,61 +1,140 @@
 import sqlite3
+from employee import Employee
 
 # sample department entries to submit into department db
-department_names = ['Administrative',
-                    'Product Development',
-                    'Sales']
+department_names = [('Administrative',),
+                    ('Product Development',),
+                    ('Sales',)]
 
 
 # sample employee entries to submit into employee db
-new_hires = [('Jane', 'Doe', 1, None, 175,000),
-             ('John', 'Smith', 1, 1, 85,000),
-             ('Samantha', 'Stevens', 1, 1, 85,000),
-             ('Paul', 'Reitgard', 2, None, 110,000),
-             ('Noah', 'Shapiro', 2, 4, 70,000),
-             ('Lena', 'Hernandez', 2, 4, 65,000),
-             ('Julia', 'Syndergaard', 2, 4, 50,000),
-             ('Ashley', 'Wu', 3, None, 125,000),
-             ('Richard', 'Wilder', 3, 8, 95,000),
-             ('Sarah', 'Stone', 3, 8, 95,000)]
+current_staff = [('Jane', 'Doe', 1, None, 175000),
+                 ('John', 'Smith', 1, 1, 85000),
+                 ('Samantha', 'Stevens', 1, 1, 85000),
+                 ('Paul', 'Reitgard', 2, None, 110000),
+                 ('Noah', 'Shapiro', 2, 4, 70000),
+                 ('Lena', 'Hernandez', 2, 4, 65000),
+                 ('Julia', 'Syndergaard', 2, 4, 50000),
+                 ('Ashley', 'Wu', 3, None, 125000),
+                 ('Richard', 'Wilder', 3, 8, 95000),
+                 ('Sarah', 'Stone', 3, 8, 95000)]
 
 
-# create connections to dbs
-conn_d = sqlite3.connect('department.db')
-cur_d = conn_d.cursor()
+# create connections to db
+def create_conn_and_cur(db_file):
+    """ create a db connection and cursor to SQLite database
+    -param db_file: database db_file
+    -return: connection object or None, cursor object or None
+    """
 
-conn_e = sqlite3.connect('employee.db')
-cur_e = conn_e.cursor()
+    try:
+        conn = sqlite3.connect(db_file)
+        c = conn.cursor()
+        return conn, c
+    except Error as e:
+        print(e)
+
+    return None, None
 
 
-# departments table creation and value entry
-cur_d.execute("""CREATE TABLE IF NOT EXISTS departments (
-                 department_id integer PRIMARY KEY,
-                 department_name text NOT NULL,
-                 FOREIGN KEY (department_id)
-                    REFERENCES employees (department_id))""")
+# table creation
+def create_table(cur, create_table_sql):
+    """ create a table in current connection using a cursor and sql prompt as
+        input
+    -param cur: cursor object of current connection
+           create_table_sql: string object of sql code to create a table
+    -return:
+    """
+    try:
+        cur.execute(create_table_sql)
+    except Error as e:
+        print(e)
 
-cur_d.executemany("INSERT INTO departments (department_name) VALUES (?)", department_names)
+
+# data entry
+def insert_departments_table(cur, entries_sql):
+    """ inserts entries into the departments tableself.
+    -param cur: cursor object of current connection
+           entries_sql: either a single tuple or list of tuples to insert
+    -return:
+    """
+
+    if isinstance(entries_sql, tuple):
+        cur.execute("INSERT INTO departments (department_name) VALUES (?)",
+                         entries_sql)
+    elif isinstance(entries_sql, list):
+        cur.executemany("INSERT INTO departments (department_name) VALUES (?)",
+                             entries_sql)
+    else:
+        print("Entries are not of proper format")
 
 
-# creating a table to store employee data
-# cur_e.execute("""CREATE TABLE employees (
-#              employee_id integer PRIMARY KEY,
-#              first text NOT NULL,
-#              last text NOT NULL,
-#              department_id integer NOT NULL,
-#              manager_id integer,
-#              pay integer NOT NULL,
-#              FOREIGN KEY (department_id)
-#                 REFERENCES departments(department_id))""")
-#
-# # inserting employees into employees table
-# cur_e.executemany("INSERT INTO employee(first, last,
-#                                     department_id,
-#                                     manager_id,
-#                                     pay)
-#                    VALUES (?, ?, ?, ?, ?)", new_hires)
+def insert_employees_table(cur, entries_sql):
+    """ inserts entries into the departments tableself.
+    -param cur: cursor object of current connection
+           entries_sql: either a single tuple or list of tuples to insert
+    -return:
+    """
 
-conn_d.commit()
-conn_d.close()
-conn_e.commit()
-conn_e.close()
+    if isinstance(entries_sql, tuple):
+        cur.execute("""INSERT INTO employees (first,
+                                              last,
+                                              department_id,
+                                              manager_id,
+                                              pay)
+                                VALUES (?, ?, ?, ?, ?)
+                        """, entries_sql)
+    elif isinstance(entries_sql, list):
+        cur.executemany("""INSERT INTO employees (first,
+                                                  last,
+                                                  department_id,
+                                                  manager_id,
+                                                  pay)
+                                VALUES (?, ?, ?, ?, ?)
+                        """, entries_sql)
+    else:
+        print("Entries are not of proper format")
+
+
+
+
+def main():
+    database = '/Users/bemiste1/Desktop/sqlite_example/employee.db'
+
+    # sql_create_departments_table = """CREATE TABLE IF NOT EXISTS departments (
+    #                                       department_id integer PRIMARY KEY,
+    #                                       department_name text NOT NULL
+    #                                    );"""
+    #
+    # sql_create_employees_table = """CREATE TABLE IF NOT EXISTS employees (
+    #                                     employee_id integer PRIMARY KEY,
+    #                                     first text NOT NULL,
+    #                                     last text NOT NULL,
+    #                                     department_id integer,
+    #                                     manager_id integer,
+    #                                     pay integer NOT NULL,
+    #                                     FOREIGN KEY (department_id)
+    #                                       REFERENCES departments (department_id)
+    #                                 );"""
+
+    # establish connection and cursor
+    conn, c = create_conn_and_cur(database)
+
+    # create tables
+    if conn is not None:
+        # create departments table
+        create_table(c, sql_create_departments_table)
+        # create employee table
+        create_table(c, sql_create_employees_table)
+    else:
+        print("Error! Failure to create database connection")
+
+    # insert initial entries
+    insert_departments_table(c, department_names)
+    insert_employees_table(c, current_staff)
+
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    main()
